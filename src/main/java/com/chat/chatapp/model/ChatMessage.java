2eebfa07f3089e88c2feec163952345ca7663561
varshithrevally <revallyvarshith989@gmail.com>
2026-03-21 17:58:41 +0530

    Initial commit

---
+++ src/main/java/com/chat/chatapp/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.chat.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
